40de3c781adaa2a6f0b91752b1e910c6e72ac6f9
Elyes Saddem <saddemelyes67@gmail.com>
2022-09-06 20:21:36 +0200

    ChangesMade

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css';
 function App() {
   return (
         <div className="App">
-          <div className="background-gray">
             <div className="p-3-white">
               <img src="images/image-qr-code.png" alt="qr-code"/>
               <h3 className="c-font-700">Improve your front-end skills by building projects</h3>
               <p className="c-font-400">Scan the QR code to visit Front End Mentor and take your coding skills to the next level</p>
-            </div>
           </div>
         </div>
   );
